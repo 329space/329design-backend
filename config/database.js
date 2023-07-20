b6880a3de6ac970be0a8f1d9ded922a5887c00a3
mynameisjisoo <devjisoolee@gmail.com>
2023-07-20 18:06:56 +0900

    aws rds 엔드포인트 적용

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "jisoo"),
-      password: env("DATABASE_PASSWORD", "0000"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: env("DATABASE_SSL", false),
     },
